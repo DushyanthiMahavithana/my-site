56f18df55254c44fd4952749532b4b09e4910098
Dushyanthi Mahavithana <115853505+DushyanthiMahavithana@users.noreply.github.com>
2023-09-19 09:14:49 +0530

    new

--- src/Helpers/Projects.js
+++ src/Helpers/Projects.js
@@ -85,7 +85,7 @@ export const Projects=[
     },
     {
         pname:'My Web Site using React framework',
-        planguage:'JavaScript,HTML, CSS',
+        planguage:'JavaScript, HTML, CSS',
         pabout:'This web application is done using react. I added all my events, experiences and all my projects and deploy react app in Github - (https://dushyanthimahavithana.github.io/my-site/)',
         pyoutubelink:'-',
         pgitlink:"https://github.com/DushyanthiMahavithana/my-site",
